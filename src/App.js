1a537a5578a5539dbc2d5c3fc75e74c20f42e4c6
mahbub hasan <mahbubhasan2001d7@gmail.com>
2022-02-28 23:50:12 +0600

    JavaScript-Expressions-in-JSX-in-ReactJs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name ="Mahbub Hasan";
+  const age = 20 ;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My Name Is {name}</h1>
+        <h4>My Age {age}</h4>
       </header>
     </div>
   );
